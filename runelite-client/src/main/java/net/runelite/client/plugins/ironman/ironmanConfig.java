513c478890728b7583c2b3d0b272a94e677db26c
DarkSlaayer <41709823+DarkSlaayer@users.noreply.github.com>
2019-04-15 00:39:04 -0500

    r

--- runelite-client/src/main/java/net/runelite/client/plugins/ironman/ironmanConfig.java
+++
@@ -1,8 +0,0 @@
-package net.runelite.client.plugins.ironman;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-
-@ConfigGroup("ironman")
-public interface ironmanConfig extends Config {
-}
